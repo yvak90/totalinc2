def3e7894853bc1ceeda3c4e5d6e4ebf4cec6754
Ubuntu <ubuntu@ip-172-31-14-5.ap-south-1.compute.internal>
2021-04-23 20:46:25 +0530

    updated printing indendation

--- pyscripts/ec2_count.py
+++ pyscripts/ec2_count.py
@@ -2,8 +2,8 @@ import boto3
 session = boto3.Session(region_name='ap-south-1')
 ec2 = session.resource('ec2')
 d = dict()
-print(" OS 	Type	Count")
-print("-----------------------------------")
+print(" OS		Type			Count")
+print("------------------------------------------------")
 for i in ec2.instances.all():
     key = "{},{}".format(i.platform,i.instance_type)
     if key in d:
@@ -13,4 +13,4 @@ for i in ec2.instances.all():
         d[key] = 1
 for x in d.keys():
     ar = x.split(",")
-    print("{}   {}    {}".format(ar[0],ar[1],d[x]));
+    print("{}		{}		{}".format(ar[0],ar[1],d[x]));
